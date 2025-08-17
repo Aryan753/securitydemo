686f60697a6a950d7a6b6efbe5fb23b91e70d6c1
Aryan753 <baliyan892329@gmail.com>
2025-08-17 14:52:15 +0530

    Role Based authorization

--- src/main/java/com/example/securitydemo/GreetingsController.java
+++ src/main/java/com/example/securitydemo/GreetingsController.java
@@ -1,12 +1,25 @@
 package com.example.securitydemo;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GreetingsController {
-    @GetMapping("hello")
+    @GetMapping("/hello")
     public String sayHello(){
         return "hello";
+    }
+
+    @PreAuthorize("hasRole('USER')")
+    @GetMapping("/user")
+    public String userEndPoint(){
+        return "hello user";
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @GetMapping("/admin")
+    public String adminEndPoint(){
+        return "hello admin";
     }
 }
